36d182284a0267a6b42d713050e7aa44994fc7d4
Walle908 <walle0908@yandex.ru>
2026-06-24 02:14:31 +0400

    feat: create next project, add ts, eslint, prettier configs

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+const eslintConfig = defineConfig([
+  globalIgnores(['node_modules/**', '.next/**', 'out/**', 'coverage/**', 'dist/**']),
+
+  ...nextVitals,
+  ...nextTs,
+
+  {
+    files: ['**/*.{ts,tsx,mts,js,jsx,mjs}'],
+    extends: [js.configs.recommended, ...tseslint.configs.strict, eslintConfigPrettier],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.vitest,
+      },
+    },
+    linterOptions: {
+      noInlineConfig: true,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+    },
+  },
+]);
+
+export default eslintConfig;
