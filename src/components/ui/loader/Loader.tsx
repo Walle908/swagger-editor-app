37ca0237624ae818e6d46d485bc9a88eb82b5000
Walle908 <walle0908@yandex.ru>
2026-06-26 18:24:36 +0400

    refactor: use self-closing tag for Loader

--- src/components/ui/loader/Loader.tsx
+++ src/components/ui/loader/Loader.tsx
@@ -2,5 +2,5 @@ import { type ReactNode } from 'react';
 import styles from './Loader.module.scss';
 
 export function Loader(): ReactNode {
-  return <div className={styles.loader} data-testid="loader-element"></div>;
+  return <div className={styles.loader} data-testid="loader-element" />;
 }
