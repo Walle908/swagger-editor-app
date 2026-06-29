42a715404accdc3800541c4c59cd3f6a7f894b51
Yuliya Aleinik <aleinikya@mail.ru>
2026-06-29 22:01:11 +0300

    feat: setup and initialize firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCb1YgYuroAqRQaB7n7_9AxEhLHsd3cgoE',
-  authDomain: 'swagger-editor-app-f3158.firebaseapp.com',
+  authDomain: '://firebaseapp.com',
   projectId: 'swagger-editor-app-f3158',
   storageBucket: 'swagger-editor-app-f3158.firebasestorage.app',
   messagingSenderId: '1056116168953',
@@ -12,4 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+export const auth = getAuth(app);
