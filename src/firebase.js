1a0740b6ea3f16be8c8fdb638e74c6a084c13bc7
Yuliya Aleinik <aleinikya@mail.ru>
2026-06-30 00:01:59 +0300

    feat: change setting db

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCb1YgYuroAqRQaB7n7_9AxEhLHsd3cgoE',
-  authDomain: '://firebaseapp.com',
-  projectId: 'swagger-editor-app-f3158',
-  storageBucket: 'swagger-editor-app-f3158.firebasestorage.app',
-  messagingSenderId: '1056116168953',
-  appId: '1:1056116168953:web:7f5b496a79f011353faa28',
-  measurementId: 'G-DS4YJF6EQ5',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
