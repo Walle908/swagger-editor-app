532d454ba4a18e0ef5de83fd870d716db8bc916c
Walle908 <walle0908@yandex.ru>
2026-07-03 23:57:11 +0400

    fix: fix Loader and LoadingPage

--- src/views/loadingPage/LoadingPage.tsx
+++ src/views/loadingPage/LoadingPage.tsx
@@ -5,8 +5,9 @@ import { Loader } from '@/components/ui';
 import styles from './LoadingPage.module.scss';
 
 export default function LoadingPage(): ReactNode {
-  <div className={styles.loadingContainer} role="status" aria-label="Loading">
-    <Loader />;
-  </div>;
-  return;
+  return (
+    <div className={styles.loadingContainer} role="status" aria-label="Loading">
+      <Loader />;
+    </div>
+  );
 }
