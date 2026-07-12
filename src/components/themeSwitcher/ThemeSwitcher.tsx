61cb6707459a07e16398408c240be225a0c7ab75
Walle908 <walle0908@yandex.ru>
2026-07-13 00:29:21 +0400

    test: add unit tests for ThemeSwitcher

--- src/components/themeSwitcher/ThemeSwitcher.tsx
+++ src/components/themeSwitcher/ThemeSwitcher.tsx
@@ -1,7 +1,6 @@
 import { ReactNode, useEffect } from 'react';
 import Image from 'next/image';
-import moon from 'public/moon.svg';
-import sunny from 'public/sunny.svg';
+
 import styles from './ThemeSwitcher.module.scss';
 import { useThemeStore } from '@/store/themeStore';
 
@@ -16,9 +15,9 @@ export default function ThemeSwitcher(): ReactNode {
   return (
     <button className={styles.themeBtn} onClick={toggleTheme} aria-label="Toggle theme">
       {isDark ? (
-        <Image src={sunny} alt="Dark theme" priority />
+        <Image src="/sunny.svg" alt="Dark theme" priority />
       ) : (
-        <Image src={moon} alt="Light theme" priority />
+        <Image src="/moon.svg" alt="Light theme" priority />
       )}
     </button>
   );
